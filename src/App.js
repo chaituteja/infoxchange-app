6c5ca9917fa0cf664d251b2080a6638ae4b4df0f
Teja <j.chaitanyateja@gmail.com>
2021-01-20 12:12:20 +1100

    Created getBooks function
    Which get books info from the db.json when component is mounted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: [],
+      loading: false,
+      bookData: {
+        title: "",
+        author: "",
+        isbn:""
+      }
+    };
+  }
+
+  // Get list of book when component is mounted
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  // Fetch call to get All books
+  getBooks() {
+    fetch("http://localhost:5000/books")
+    .then(resp => resp.json())
+    .then(data => {
+        this.setState({ books: data, loading: false})
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
